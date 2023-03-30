5ec06cb1c805493d1215d6990e118b548a5febe7
Vojta Tomas <hello@vojtatom.cz>
2023-03-30 16:36:27 +0200

    Updated worker

--- src/Workers/IFC.worker.ts
+++ src/Workers/IFC.worker.ts
@@ -1,49 +1,19 @@
-import { TypedArray, UserInputModel } from 'types';
+import { UserInputModel } from 'types';
 
 import { IFCManager } from './IFC/components/IFCManager';
 import { IFCModel, IFCModelData } from './IFC/components/IFCModel';
 
-class IFCLoader {
-    readonly data: { [item: number]: TypedArray } = {};
-    readonly wasmPath = '/';
-
-    async loadIFC(name: string, buffer: ArrayBuffer) {
-        const manager = new IFCManager();
-        await manager.setWasmPath(this.wasmPath, true);
-        const model = await manager.parse(buffer);
-        const models = this.flattenModelTree(model);
-        return models;
-    }
-
-    private flattenModelTree(model: IFCModel) {
-        const flat: IFCModelData[] = [model.toIFCModelData()];
-        model.children.forEach((child) => {
-            flat.push(...this.flattenModelTree(child as IFCModel));
-        });
-        return flat;
-    }
-
-    static transferable(models: IFCModelData[]) {
-        const transferable: Transferable[] = [];
-        models.forEach((model) => {
-            if (model.geometry.expressID) transferable.push(model.geometry.expressID.buffer);
-            if (model.geometry.position) transferable.push(model.geometry.position.buffer);
-            if (model.geometry.normal) transferable.push(model.geometry.normal.buffer);
-            if (model.geometry.index) transferable.push(model.geometry.index.buffer);
-        });
-        return transferable;
-    }
-}
+self.onmessage = (e) => {
+    parseIFC(e.data);
+};
 
 export function parseIFC(models: UserInputModel[]) {
     console.log(`Loading ${models.length} models`);
     models.forEach(async (model) => {
         if (model.name.endsWith('.ifc')) {
             console.log(`Loading IFC model ${model.name}`);
-            const ifcLoader = new IFCLoader();
-            const models = await ifcLoader.loadIFC(model.name, model.buffer);
-            const transferable = IFCLoader.transferable(models);
-            console.log(models);
+            const models = await loadIFC(model.buffer);
+            const transferable = getTransferable(models);
             (self as any).postMessage(
                 {
                     name: model.name,
@@ -55,6 +25,31 @@ export function parseIFC(models: UserInputModel[]) {
     });
 }
 
-self.onmessage = (e) => {
-    parseIFC(e.data);
-};
+const wasmPath = '/';
+
+async function loadIFC(buffer: ArrayBuffer) {
+    const manager = new IFCManager();
+    await manager.setWasmPath(wasmPath, true);
+    const model = await manager.parse(buffer);
+    const models = flattenModelTree(model);
+    return models;
+}
+
+function flattenModelTree(model: IFCModel) {
+    const flat: IFCModelData[] = [model.toIFCModelData()];
+    model.children.forEach((child) => {
+        flat.push(...flattenModelTree(child as IFCModel));
+    });
+    return flat;
+}
+
+function getTransferable(models: IFCModelData[]) {
+    const transferable: Transferable[] = [];
+    models.forEach((model) => {
+        if (model.geometry.expressID) transferable.push(model.geometry.expressID.buffer);
+        if (model.geometry.position) transferable.push(model.geometry.position.buffer);
+        if (model.geometry.normal) transferable.push(model.geometry.normal.buffer);
+        if (model.geometry.index) transferable.push(model.geometry.index.buffer);
+    });
+    return transferable;
+}
