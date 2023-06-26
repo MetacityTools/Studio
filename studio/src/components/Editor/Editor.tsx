01643dde208a9c0492921e1d6a67ee9fe90d681c
Vojta Tomas <hello@vojtatom.cz>
2023-06-26 16:02:27 +0200

    Switched sides back

--- studio/src/components/Editor/Editor.tsx
+++ studio/src/components/Editor/Editor.tsx
@@ -16,17 +16,17 @@ export function ModelEditor() {
         <SizeGuard minWidth={600} minHeight={400}>
             <div className="w-full h-full">
                 <Allotment separator={false}>
-                    <Allotment.Pane
-                        minSize={500}
-                        preferredSize={500}
-                        className="border-r border-neutral-200"
-                    >
-                        <SidePanel />
-                    </Allotment.Pane>
                     <Allotment.Pane minSize={200} className="bg-neutral-100">
                         <CanvasComponent />
                         <Help />
                         <Controls />
+                    </Allotment.Pane>
+                    <Allotment.Pane
+                        minSize={500}
+                        preferredSize={500}
+                        className="border-l border-neutral-200"
+                    >
+                        <SidePanel />
                     </Allotment.Pane>
                 </Allotment>
             </div>
