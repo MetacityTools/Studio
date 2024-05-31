8842b93730932f4af1f7dfb634b077a713b2ba9e
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-05-31 12:19:53 +0000

    tests: simplify tests

--- studio/features/models/index.test.ts
+++ studio/features/models/index.test.ts
@@ -1,40 +1,38 @@
-import { describe, expect, test } from "vitest";
+import { expect, test } from "vitest";
 import { createOwnModel, deleteOwnModel, getOwnModel } from ".";
 import { Model } from "../db/entities/model";
 
-describe("model actions", () => {
-  const modelFile = {
-    stream: () =>
-      new ReadableStream({
-        start(controller) {
-          controller.enqueue(new TextEncoder().encode("test"));
-          controller.close();
-        },
-      }),
-    name: "test.txt",
-  } as File;
+const modelFile = {
+  stream: () =>
+    new ReadableStream({
+      start(controller) {
+        controller.enqueue(new TextEncoder().encode("test"));
+        controller.close();
+      },
+    }),
+  name: "test.txt",
+} as File;
 
-  const modelMetadata: Pick<Model, "name" | "coordinateSystem"> = {
-    name: "My best model",
-    coordinateSystem: "WGS84",
-  };
+const modelMetadata: Pick<Model, "name" | "coordinateSystem"> = {
+  name: "My best model",
+  coordinateSystem: "WGS84",
+};
 
-  test("model CRD", async () => {
-    let model: Model | null;
+test("model CRD", async () => {
+  let model: Model | null;
 
-    // CREATE
-    model = await createOwnModel(modelMetadata, [modelFile]);
-    expect(model).toMatchObject(modelMetadata);
+  // CREATE
+  model = await createOwnModel(modelMetadata, [modelFile]);
+  expect(model).toMatchObject(modelMetadata);
 
-    // READ
-    model = await getOwnModel(model.id);
-    expect(model).toMatchObject(modelMetadata);
-    expect(model?.files).toHaveLength(1);
-    expect(model?.files?.[0]).toBe(modelFile.name);
+  // READ
+  model = await getOwnModel(model.id);
+  expect(model).toMatchObject(modelMetadata);
+  expect(model?.files).toHaveLength(1);
+  expect(model?.files?.[0]).toBe(modelFile.name);
 
-    // DELETE
-    await deleteOwnModel(model!.id);
-    model = await getOwnModel(model!.id);
-    expect(model).toBe(null);
-  });
+  // DELETE
+  await deleteOwnModel(model!.id);
+  model = await getOwnModel(model!.id);
+  expect(model).toBe(null);
 });
--- studio/features/projects/index.test.ts
+++ studio/features/projects/index.test.ts
@@ -1,36 +1,32 @@
-import { describe, expect, test } from "vitest";
+import { expect, test } from "vitest";
 import { createProject, deleteProject, getProjectById, updateProject } from ".";
 import { Project } from "../db/entities/project";
 
-describe("project actions", () => {
-  let projectId: number;
+test("project CRUD", async () => {
+  let project: Project | null;
 
-  test("project CRUD", async () => {
-    // CREATE
-    let project: Project | null = await createProject({
-      name: "Test Project",
-      description: "This is a test project",
-    });
-
-    projectId = project.id;
-
-    // READ
-    project = await getProjectById(projectId);
-    expect(project).toMatchObject({
-      name: "Test Project",
-      description: "This is a test project",
-    });
-
-    // UPDATE
-    const updatedProject = await updateProject(projectId, {
-      description: "This is an updated test project",
-    });
-
-    expect(updatedProject?.description).toBe("This is an updated test project");
-
-    // DELETE
-    await deleteProject(projectId);
-
-    expect(await getProjectById(projectId)).toBe(null);
+  // CREATE
+  project = await createProject({
+    name: "Test Project",
+    description: "This is a test project",
   });
+
+  // READ
+  project = await getProjectById(project.id);
+  expect(project).toMatchObject({
+    name: "Test Project",
+    description: "This is a test project",
+  });
+
+  // UPDATE
+  project = await updateProject(project!.id, {
+    description: "This is an updated test project",
+  });
+
+  expect(project?.description).toBe("This is an updated test project");
+
+  // DELETE
+  await deleteProject(project!.id);
+
+  expect(await getProjectById(project!.id)).toBe(null);
 });
