05bf779b248a7d4c0c23374bf3f728b54509670c
Martin Kopeček <martin.kopecek@smallhill.cz>
2024-06-21 08:58:55 +0000

    refactor: move remaining tests to tests folder

--- studio/tests/models-route.test.ts
+++ studio/tests/models-route.test.ts
@@ -1,9 +1,10 @@
 "use server";
 
+import { GET } from "@app/api/models/[model]/data/[file]/route";
 import { deleteModel } from "@features/models/mutations/deleteModel";
 import { expect } from "vitest";
-import { testWithFixtures } from "../../../tests/helpers";
-import { POST } from "./route";
+import { POST } from "../app/api/models/route";
+import { testWithFixtures } from "./helpers";
 
 testWithFixtures("POST /models", async ({ user }) => {
   const url = new URL("http://localhost:3000/models");
@@ -34,4 +35,24 @@ testWithFixtures("POST /models", async ({ user }) => {
   });
 
   await deleteModel(body.id);
+});
+
+testWithFixtures("GET /models/[model]/data/[file]", async ({ model, file }) => {
+  const url = new URL(
+    `http://localhost:3000/models/${model.id}/data/${file.name}`,
+  );
+
+  const req = new Request(url, {
+    method: "GET",
+  });
+
+  const response = await GET(req, {
+    params: { model: String(model.id), file: file.name },
+  });
+  const body = await response.text();
+
+  expect(response.status).toBe(200);
+  expect(body).toBe("test");
+
+  await deleteModel(model.id);
 });
