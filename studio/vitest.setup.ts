d9e6d925dc1b684602aab5a41ebea699a9dc9ea3
Martin Kopeček <github@smallhill.cz>
2024-06-21 13:03:14 +0200

    refator(tests): add test fixtures, move into one dir (#58)
    Co-authored-by: Martin Kopeček <martin.kopecek@smallhill.cz>

--- studio/vitest.setup.ts
+++ studio/vitest.setup.ts
@@ -1,37 +1,20 @@
 import "reflect-metadata";
-import { afterAll, beforeAll, vi } from "vitest";
-import { User } from "./features/db/entities/user";
-import { injectRepository } from "./features/db/helpers";
 
-beforeAll(async () => {
-  const userRepository = await injectRepository(User);
+import { beforeAll, vi } from "vitest";
 
-  await userRepository.save({
-    id: "test",
-    email: "test@test",
-    picture: "https://test.com/test.jpg",
-    projects: [],
-    idAuth0: "test",
-    enabled: true,
-  });
+ beforeAll(async ({}) => {
+   vi.mock("@auth0/nextjs-auth0", () => ({
+     getSession: async () => ({
+       user: {
+         sub: "test",
+         email: "test@test",
+         picture: "https://example.com/picture.png",
+         idAuth0: "test",
+         enabled: true,
+       },
+     }),
+   }));
+ });
 
-  vi.mock("@auth0/nextjs-auth0", () => ({
-    getSession: async () => ({
-      user: {
-        sub: "test",
-        email: "test@test",
-        picture: "https://example.com/picture.png",
-        idAuth0: "test",
-        enabled: true,
-      },
-    }),
-  }));
-});
 
-afterAll(async () => {
-  const userRepository = await injectRepository(User);
 
-  await userRepository.delete({
-    id: "test",
-  });
-});
