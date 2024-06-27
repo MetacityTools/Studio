d5dcffab6c95989b7e12f87f0cd918d4d58aa20c
AdamPospisil <aposp89@gmail.com>
2024-06-27 12:37:03 +0000

    feat: coordinate conversion for geojson files

--- coordinates_converter/main.py
+++ coordinates_converter/main.py
@@ -12,18 +12,14 @@ from uuid import uuid4
 app = FastAPI()
 logger = logging.getLogger('uvicorn.error')
 
-@app.post("/convert_shapefile")
-async def create_upload_file(
-    file: UploadFile, 
-    crsTarget: Union[str, None] = None, 
-    crsSource: Union[str, None] = None):
-
+def convert_gdf(gdf, crsTarget, crsSource, filename):
     if not crsTarget:
         logger("No target crs provided, using EPSG:4326 as fallback")
         crsTarget = "EPSG:4326"
-    source_filename = file.filename.split(".")[0]
 
-    gdf = gpd.read_file(file.file)
+    source_filename = filename.split(".")[0]
+    converted_filename = f"{source_filename}_{crsTarget.replace(':', '').lower()}_{uuid4()}"
+
     if gdf.crs is None:
         logger.info("No CRS found in the input file, using crsSource if provided")
         if not crsSource:
@@ -33,21 +29,62 @@ async def create_upload_file(
         except CRSError as e:
             logger.error(e)
             raise HTTPException(status_code=400, detail=f"Invalid source CRS: {crsSource}")
+    else:
+        logger.info(f"Input file has CRS: {gdf.crs}")
     try:
         gdf_converted = gdf.to_crs(crsTarget)
     except CRSError as e:
         logger.error(e)
         raise HTTPException(status_code=400, detail=f"Invalid target CRS: {crsSource}")
-    
-    converted_shapefile = f"{source_filename}_{crsTarget.replace(':', '').lower()}_{uuid4()}"
-    logger.info(f"Saving converted shapefile to {converted_shapefile}")
-    gdf_converted.to_file(converted_shapefile)
-    shutil.make_archive(converted_shapefile, 'zip', converted_shapefile)
+    return gdf_converted, converted_filename
 
-    converted_shapefile_archive = f"{converted_shapefile}.zip"
+@app.post("/convert_geojson")
+async def convert_geojson(
+    file: UploadFile, 
+    crsTarget: Union[str, None] = None, 
+    crsSource: Union[str, None] = None):
+
+    gdf_converted, converted_filename = convert_gdf(
+        gpd.read_file(file.file), 
+        crsTarget, 
+        crsSource, 
+        file.filename)
+    
+    converted_geojson = f"{converted_filename}.geojson"   
+    gdf_converted.to_file(converted_geojson, driver="GeoJSON")
 
     def cleanup():
-        shutil.rmtree(converted_shapefile)
+        logger.info("Cleaning up")
+        os.remove(converted_geojson)
+
+    return FileResponse(
+        converted_geojson, 
+        media_type="application/octet-stream", 
+        filename=converted_geojson,
+        background=BackgroundTask(cleanup))
+
+
+@app.post("/convert_shapefile")
+async def convert_shapefile(
+    file: UploadFile, 
+    crsTarget: Union[str, None] = None, 
+    crsSource: Union[str, None] = None):
+
+    gdf_converted, converted_filename = convert_gdf(
+        gpd.read_file(file.file), 
+        crsTarget, 
+        crsSource, 
+        file.filename)
+    
+    logger.info(f"Saving converted shapefile to {converted_filename}")
+    gdf_converted.to_file(converted_filename)
+    shutil.make_archive(converted_filename, 'zip', converted_filename)
+    converted_shapefile_archive = f"{converted_filename}.zip"
+
+
+    def cleanup():
+        logger.info("Cleaning up")
+        shutil.rmtree(converted_filename)
         os.remove(converted_shapefile_archive)
 
     return FileResponse(
