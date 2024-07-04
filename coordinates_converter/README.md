f132ad30aecd55cfe170e62249de366886852eb1
AdamPospisil <aposp89@gmail.com>
2024-07-04 14:05:24 +0000

    feat: gltf transform support

--- coordinates_converter/README.md
+++ coordinates_converter/README.md
@@ -1,17 +1,30 @@
 # Metacity Studio Coordinates Converter Utility
 
 ## How to install
-* `python get_pip.py` if no pip :'(
-* `pip install -r requirements.txt` to install dependencies
+
+- `python get_pip.py` if no pip :'(
+- `pip install -r requirements.txt` to install dependencies
 
 ## How to run
-* `fastapi dev main.py` to run on localhost
+
+- `fastapi dev main.py` to run on localhost
 
 ## How to use (Shapefile)
-* example curl - shapefile with projection:
-  * `curl -F "file=@shapefile.zip" -X POST http://localhost:8000/uploadfile?crsTarget=EPSG:4326`
-* example curl - shapefile without projection:
-  * `curl -F "file=@shapefile_noproj.zip" -X POST 'http://localhost:8000/convert_shapefile?crsTarget=EPSG:4326&crsSource=EPSG:5514'`
-* `file` - zipped shapefile
-* `crsTarget` - EPSG code of coordinate system to convert to
-* `crsSource` - EPSGE code of initial coordinate system (if shapefile doesn't contain projection info)
+
+- example curl - shapefile with projection:
+  - `curl -F "file=@shapefile.zip" -X POST http://localhost:8000/convert_shapefile?crsTarget=EPSG:4326`
+- example curl - shapefile without projection:
+  - `curl -F "file=@shapefile_noproj.zip" -X POST 'http://localhost:8000/convert_shapefile?crsTarget=EPSG:4326&crsSource=EPSG:5514'`
+- `file` - zipped shapefile
+- `crsTarget` - EPSG code of coordinate system to convert to
+- `crsSource` - EPSG code of initial coordinate system (if shapefile doesn't contain projection info)
+
+## How to use (GeoJSON)
+
+- example curl - GeoJSON with projection:
+  - `curl -F "file=@data.geojson" -X POST http://localhost:8000/convert_geojson?crsTarget=EPSG:4326`
+- example curl - GeoJSON without projection:
+  - `curl -F "file=@data_noproj.geojson" -X POST 'http://localhost:8000/convert_geojson?crsTarget=EPSG:4326&crsSource=EPSG:5514'`
+- `file` - input geojson
+- `crsTarget` - EPSG code of coordinate system to convert to
+- `crsSource` - EPSG code of initial coordinate system (if shapefile doesn't contain projection info)
--- coordinates_converter/main.py
+++ coordinates_converter/main.py
@@ -1,16 +1,21 @@
-import geopandas as gpd
 import logging
 import os
 import shutil
-from fastapi import FastAPI, UploadFile, HTTPException
-from pyproj.exceptions import CRSError
-from starlette.background import BackgroundTask
-from starlette.responses import FileResponse
 from typing import Union
 from uuid import uuid4
 
+import geopandas as gpd
+import pygltflib
+from fastapi import FastAPI, HTTPException, UploadFile
+from gltf import gltf_transform
+from pyproj.exceptions import CRSError
+from starlette.background import BackgroundTask
+from starlette.responses import FileResponse
+from pyproj import CRS, Transformer
+
 app = FastAPI()
-logger = logging.getLogger('uvicorn.error')
+logger = logging.getLogger("uvicorn.error")
+
 
 def convert_gdf(gdf, crsTarget, crsSource, filename):
     if not crsTarget:
@@ -18,17 +23,24 @@ def convert_gdf(gdf, crsTarget, crsSource, filename):
         crsTarget = "EPSG:4326"
 
     source_filename = filename.split(".")[0]
-    converted_filename = f"{source_filename}_{crsTarget.replace(':', '').lower()}_{uuid4()}"
+    converted_filename = (
+        f"{source_filename}_{crsTarget.replace(':', '').lower()}_{uuid4()}"
+    )
 
     if gdf.crs is None:
         logger.info("No CRS found in the input file, using crsSource if provided")
         if not crsSource:
-            raise HTTPException(status_code=400, detail="No CRS found in the input file and no crsSource provided")        
+            raise HTTPException(
+                status_code=400,
+                detail="No CRS found in the input file and no crsSource provided",
+            )
         try:
             gdf.crs = crsSource
         except CRSError as e:
             logger.error(e)
-            raise HTTPException(status_code=400, detail=f"Invalid source CRS: {crsSource}")
+            raise HTTPException(
+                status_code=400, detail=f"Invalid source CRS: {crsSource}"
+            )
     else:
         logger.info(f"Input file has CRS: {gdf.crs}")
     try:
@@ -38,19 +50,17 @@ def convert_gdf(gdf, crsTarget, crsSource, filename):
         raise HTTPException(status_code=400, detail=f"Invalid target CRS: {crsSource}")
     return gdf_converted, converted_filename
 
+
 @app.post("/convert_geojson")
 async def convert_geojson(
-    file: UploadFile, 
-    crsTarget: Union[str, None] = None, 
-    crsSource: Union[str, None] = None):
+    file: UploadFile, crsTarget: str, crsSource: Union[str, None] = None
+):
 
     gdf_converted, converted_filename = convert_gdf(
-        gpd.read_file(file.file), 
-        crsTarget, 
-        crsSource, 
-        file.filename)
-    
-    converted_geojson = f"{converted_filename}.geojson"   
+        gpd.read_file(file.file), crsTarget, crsSource, file.filename
+    )
+
+    converted_geojson = f"{converted_filename}.geojson"
     gdf_converted.to_file(converted_geojson, driver="GeoJSON")
 
     def cleanup():
@@ -58,29 +68,26 @@ async def convert_geojson(
         os.remove(converted_geojson)
 
     return FileResponse(
-        converted_geojson, 
-        media_type="application/octet-stream", 
+        converted_geojson,
+        media_type="application/octet-stream",
         filename=converted_geojson,
-        background=BackgroundTask(cleanup))
+        background=BackgroundTask(cleanup),
+    )
 
 
 @app.post("/convert_shapefile")
 async def convert_shapefile(
-    file: UploadFile, 
-    crsTarget: Union[str, None] = None, 
-    crsSource: Union[str, None] = None):
+    file: UploadFile, crsTarget: str, crsSource: Union[str, None] = None
+):
 
     gdf_converted, converted_filename = convert_gdf(
-        gpd.read_file(file.file), 
-        crsTarget, 
-        crsSource, 
-        file.filename)
-    
+        gpd.read_file(file.file), crsTarget, crsSource, file.filename
+    )
+
     logger.info(f"Saving converted shapefile to {converted_filename}")
     gdf_converted.to_file(converted_filename)
-    shutil.make_archive(converted_filename, 'zip', converted_filename)
+    shutil.make_archive(converted_filename, "zip", converted_filename)
     converted_shapefile_archive = f"{converted_filename}.zip"
-
 
     def cleanup():
         logger.info("Cleaning up")
@@ -88,7 +95,48 @@ async def convert_shapefile(
         os.remove(converted_shapefile_archive)
 
     return FileResponse(
-        converted_shapefile_archive, 
-        media_type="application/octet-stream", 
+        converted_shapefile_archive,
+        media_type="application/octet-stream",
         filename=converted_shapefile_archive,
-        background=BackgroundTask(cleanup))
+        background=BackgroundTask(cleanup),
+    )
+
+
+@app.post("/convert_gltf")
+async def convert_gltf(file: UploadFile, crsTarget: str, crsSource: str):
+    # logger.info(f"Converting {file.filename} from {crsSource} to {crsTarget}")
+    name, suffix = file.filename.split(".")
+    input_name = f"{name}_{uuid4()}.gltf"
+
+    transformer = Transformer.from_crs(
+        CRS.from_string(crsSource), CRS.from_string(crsTarget)
+    )
+
+    if suffix != "glb":
+        with open(input_name, "w") as input_file:
+            input_file.write(file.file.read().decode("utf-8"))
+    else:
+        with open(input_name, "wb") as input_file:
+            input_file.write(file.file.read())
+
+    gltf = (
+        pygltflib.GLTF2().load(input_name)
+        if suffix != "glb"
+        else pygltflib.GLTF2().load_binary(input_name)
+    )
+    gltf_output = gltf_transform(gltf, transformer)
+
+    output_name = f"out_{input_name}"
+    gltf_output.save(output_name)
+
+    def cleanup():
+        logger.info("Cleaning up")
+        os.remove(input_name)
+        os.remove(output_name)
+
+    return FileResponse(
+        output_name,
+        media_type="application/octet-stream",
+        filename="result.gltf",
+        background=BackgroundTask(cleanup),
+    )
