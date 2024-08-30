ae3fc4422d6f5f7ca19b8afab1fd3715f6856e37
Vojta Tomas <hello@vojtatom.cz>
2024-08-30 11:53:17 +0000

    chore: remove iconify

--- studio/core/icons/Icon.tsx
+++ studio/core/icons/Icon.tsx
@@ -1,24 +1,5 @@
-import {
-  Icon as AdobeIcon,
-  IconProps as AdobeIconProps,
-} from "@adobe/react-spectrum";
-import {
-  enableCache,
-  Icon as IconifyIcon,
-  IconProps as IconifyIconProps,
-} from "@iconify/react";
+import { IconProps as AdobeIconProps } from "@adobe/react-spectrum";
 import { SVGProps } from "react";
-
-// Enable caching in localStorage
-enableCache("local");
-
-export function Icon(props: IconifyIconProps) {
-  return (
-    <AdobeIcon>
-      <IconifyIcon {...props} />
-    </AdobeIcon>
-  );
-}
 
 export type IconProps = Omit<AdobeIconProps, "children"> & {
   transform?: SVGProps<SVGSVGElement>["transform"];
